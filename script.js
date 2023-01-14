622e8be6dbbefb9c011e91c28d60102f44afcf19
alikumaail <alikumail1100@gmail.com>
2023-01-13 23:31:20 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const crossbar = document.getElementById("crossbar");
+const crossbarLogin = document.getElementById("crossbarLogin");
+const joinNow = document.getElementById("joinNow");
+const loginBtn = document.getElementById("loginBtn");
+
+const joinNowM = document.getElementById("joinNowM");
+const loginBtnM = document.getElementById("loginBtnM");
+
+const loginModal = document.getElementById("loginModal");
+const joinModal = document.getElementById("joinModal");
+
+crossbar.addEventListener("click", () => {
+    // if (crossbar.classList.contains("displayNav")) {
+
+    joinModal.classList.remove("displayNav");
+    document.body.style.overflow = "auto";
+
+    console.log("crossbar");
+
+    // }
+});
+crossbarLogin.addEventListener("click", () => {
+    // if (crossbar.classList.contains("displayNav")) {
+
+    loginModal.classList.remove("displayNav");
+    document.body.style.overflow = "auto";
+
+    console.log("crossbar");
+
+    // }
+});
+
+joinNow.addEventListener("click", () => {
+    // if (loginModal.classList.contains("displayNav")) {
+    joinModal.classList.add("displayNav");
+    document.body.style.overflow = "hidden";
+    // }
+    console.log("joinnow");
+});
+loginBtn.addEventListener("click", () => {
+    // if (loginModal.classList.contains("displayNav")) {
+    loginModal.classList.add("displayNav");
+    document.body.style.overflow = "hidden";
+    // }
+    console.log("joinnow");
+});
+
+joinNowM.addEventListener("click", () => {
+    // if (loginModal.classList.contains("displayNav")) {
+    joinModal.classList.add("displayNav");
+    document.body.style.overflow = "hidden";
+    // }
+    console.log("joinnow");
+});
+loginBtnM.addEventListener("click", () => {
+    // if (loginModal.classList.contains("displayNav")) {
+    loginModal.classList.add("displayNav");
+    document.body.style.overflow = "hidden";
+    // }
+    console.log("joinnow");
+});
+
+const hamburger = document.getElementById("hamburger");
+const mobileNav = document.getElementById("mobileNav");
+const navCross = document.getElementById("navCross");
+
+hamburger.addEventListener("click", () => {
+    mobileNav.style.display = "flex";
+    console.log("ham");
+});
+navCross.addEventListener("click", () => {
+    mobileNav.style.display = "none";
+});
