f21d9ffef4834187f8af244274343bf4285ca002
alikumaail <alikumail1100@gmail.com>
2023-01-23 02:33:22 +0500

    portal avatar

--- portalScript.js
+++ portalScript.js
@@ -10,3 +10,31 @@ hamburgerPortal.addEventListener("click", () => {
 navCrossPortal.addEventListener("click", () => {
     mobileNavPortal.style.display = "none";
 });
+
+const avatarStats = document.getElementById("avatarStats");
+const avatarItems = document.getElementById("avatarItems");
+
+const avatarStatsButton = document.getElementById("avatarStatsButton");
+const avatarItemsButton = document.getElementById("avatarItemsButton");
+console.log(avatarItems);
+
+avatarItemsButton.addEventListener('click',()=>{
+    console.log("stats");
+    if(!avatarItems.classList.contains('avatarShow')){
+        avatarItems.classList.add('avatarShow');
+        avatarItemsButton.classList.add('avatar-info-current');
+        avatarStatsButton.classList.remove('avatar-info-current');
+        avatarStats.classList.remove('avatarShow');
+    }
+})
+
+avatarStatsButton.addEventListener('click',()=>{
+    console.log("items");
+
+    if(!avatarStats.classList.contains('avatarShow')){
+        avatarStats.classList.add('avatarShow');
+        avatarStatsButton.classList.add('avatar-info-current');
+        avatarItemsButton.classList.remove('avatar-info-current');
+        avatarItems.classList.remove('avatarShow');
+    }
+})
