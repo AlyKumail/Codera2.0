d924b1ba74cd1a67ccd259d6ab41212784cb7d20
alikumaail <alikumail1100@gmail.com>
2023-02-05 19:51:56 +0500

    revisions done

--- dashboardScript.js
+++ dashboardScript.js
@@ -9,7 +9,31 @@ viewBtn.addEventListener("click", () => {
 acheivementCross.addEventListener("click", () => {
     acheivementModal.style.display = "none";
 });
-
+ 
 {
     /* <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>; */
 }
+
+const badges = document.querySelectorAll('.badge');
+const badgePopup = document.querySelector('#badgePopup');
+
+badges.forEach(badge=>{
+    console.log(badge);
+    badge.addEventListener('mouseover',(e)=>{
+        console.log(e.clientY)
+        // badgePopup.classList.add('show')
+        let top = e.clientY + 'px';
+        let left = e.clientX + 'px';
+        badgePopup.style.top = top;
+        badgePopup.style.left = left;
+        badgePopup.style.display = 'block';
+
+    })
+    badge.addEventListener('mouseleave',(e)=>{
+        // console.log(e.target.classList.add('show'))
+        // badgePopup.classList.remove('show')
+        badgePopup.style.display = 'none';
+
+        // badgePopup.style.top
+    })
+})
--- quiz.js
+++ quiz.js
@@ -1,38 +1,13 @@
 const answersBox = document.querySelectorAll('.answer-box');
 const answers = document.querySelectorAll('.answer');
+const quizAnswersWrap = document.querySelectorAll('.quiz-answers-wrap');
 const quiz = document.querySelector('.quiz');
 const quizAnswers = document.querySelector('.quiz-answers');
-
-// quiz.addEventListener('dragenter',e=>{
-//     e.preventDefault();
-   
-// })
-// quiz.addEventListener('dragover',e=>{
-//     e.preventDefault();
-    
-// })
-// quiz.addEventListener('drop',e=>{
-//     // e.target.classList.remove('bgHover');
-    
-
-//     // get the draggable element
-//     const id = e.dataTransfer.getData('text/plain');
-//     const draggable = document.getElementById(id);
-
-//     quizAnswers.appendChild(draggable);
-
-//     // display the draggable element
-//     draggable.classList.remove('hide');
-// })
 
 answers.forEach(answer=>{
     answer.addEventListener('dragstart',e=>{
         console.log(e.target);
         e.dataTransfer.setData('text/plain', e.target.id);
-        setTimeout(() => {
-            e.target.classList.add('blueBorder');
-            e.target.classList.add('hide');
-        }, 0);
     })
 })
 
@@ -55,12 +30,42 @@ answersBox.forEach(answerBox=>{
         // get the draggable element
         const id = e.dataTransfer.getData('text/plain');
         const draggable = document.getElementById(id);
+        draggable.classList.add('blueBorder');
+        draggable.classList.add('boxDropped');
 
         e.target.appendChild(draggable);
-
         // display the draggable element
         draggable.classList.remove('hide');
     })
 })
+
+quizAnswers.addEventListener('dragenter',e=>{
+    e.preventDefault();
+    console.log('quizAnswers dragenter')
+    e.target.classList.add('bgHover');
+})
+quizAnswers.addEventListener('dragover',e=>{
+    e.preventDefault();
+    e.target.classList.add('bgHover');
+})
+quizAnswers.addEventListener('dragleave',e=>{
+    e.target.classList.remove('bgHover');
+})
+quizAnswers.addEventListener('drop',e=>{
+    e.target.classList.remove('bgHover');
+    
+
+    // get the draggable element
+    const id = e.dataTransfer.getData('text/plain');
+    const draggable = document.getElementById(id);
+    draggable.classList.remove('blueBorder');
+    draggable.classList.remove('boxDropped');
+
+    e.target.appendChild(draggable);
+
+    // display the draggable element
+    draggable.classList.remove('hide');
+})
+
 
 
